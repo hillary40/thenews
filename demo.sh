38962ae7ec6b079dbe7dbbfffc04d96a3bb6b647
hillary40 <hey.hillary.001@gmail.com>
2024-08-05 10:31:57 +0100

    the prime commit

---
+++ demo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read -p "Enter your name" name
+read -p "How old are you?" age
+read -p "What is your year of birth?" year
+read -p "Enter your gender" gender
+echo "Your name is $name, You are $age years old, You were born in $year and you are a $gender"
